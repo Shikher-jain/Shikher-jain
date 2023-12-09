087fa631b3ce9c3f8b1eea00c0e8ec7dc615428a
Shikher Jain <147051960+Shikher-jain@users.noreply.github.com>
2023-12-09 13:01:54 +0000

    function add

--- code/.vscode/python/new.py
+++ code/.vscode/python/new.py
@@ -1,9 +1,25 @@
 x=["1","hi","!","by","6",".git"]
-print(x)
+print("Before sort",x)
 x.sort()
-print(x)
-
+print("After sort",x)
 '''
 O/P:-
 x=["!",".git","1","6","by","hi"]
 '''
+def f2():
+    print("f2 is",x)
+    
+def f1():
+    x=[1,2,3,4,5,6,7,8,9,0]
+    print("f1 is",x)
+    f2()
+f1()
+def f4():
+    print("f4 is",x)
+    
+def f3():
+    global x
+    x=[1,2,3,4,5,6,7,8,9,0]
+    print("f3 is",x)
+    f4()
+f3()
