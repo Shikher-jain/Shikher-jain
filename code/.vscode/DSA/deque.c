7f1501cf17e6be8f0291c3e1a602f47e3ac5208b
Shikher-jain <shikherjain786@gmail.com>
2024-01-01 23:36:58 +0530

    deque

--- code/.vscode/DSA/deque.c
+++ code/.vscode/DSA/deque.c
@@ -1,129 +1,127 @@
-#include <stdio.h>    
-#define size 5  
-int deque[size];    
-int f = -1, r = -1;    
+#include <stdio.h>  
+#include <stdlib.h>  
+#define size 10
+int deque[size],f = -1,r = -1; 
+
 //  insert_front function will insert the value from the front    
-void insert_front(int x)    
-{    
-    if((f==0 && r==size-1) || (f==r+1))
-        printf("Overflow"); 
+void insert_front(int n) //#1   
+{   
+    int x;
+    if((f==0 && r==n-1) || (f==r+1))     
+    printf("Overflow"); 
+    else if(f==0)
+        printf("Queue isn't able to insert more value from front\n");
     else if((f==-1) && (r==-1)){    
         f=r=0;    
+        printf("Enter the digit you want to insert ");
+        scanf("%d",&x);
         deque[f]=x;    
-    }    
-    // else if(f==0) //circular queue   
-    // {    
-    //     f=size-1;    
-    //     deque[f]=x;    
-    // }    
+    }
     else{
+        printf("Enter the digit you want to insert ");
+        scanf("%d",&x);
         f=f-1;    
         deque[f]=x;    
     }    
 }    
-    
+   
 // insert_rear function will insert the value from the rear    
-void insert_rear(int x)    
+void insert_rear(int n) //#2   
 {    
-    if((f==0 && r==size-1) || (f==r+1))    
-    {    
+    int x;
+    if((f==0 && r==n-1) || (f==r+1))    
         printf("Overflow");    
-    }    
-    else if((f==-1) && (r==-1))    
-    {    
-        r=0;    
+    else if (r==n-1)
+        printf("Queue isn't able to insert more value from rear\n");
+    else if((f==-1) && (r==-1)){    
+        f=r=0;   
+        printf("Enter the digit you want to insert ");
+        scanf("%d",&x); 
         deque[r]=x;    
     }    
-    // else if(r==size-1)    
-    // { //circular queue   
-    //     r=0;    
-    //     deque[r]=x;    
-    // }    
-    else    
-    {    
+        else{    
+        printf("Enter the digit you want to insert ");
+        scanf("%d",&x);
         r++;    
         deque[r]=x;    
-    }    
-    
-}    
-    
-// display function prints all the value of deque.    
-void display()    
-{    
-    int i=f;    
-    printf("\nElements in a deque are: ");    
-        
-    while(i!=r)    
-    {    
-        printf("%d ",deque[i]);    
-        i=(i+1)%size;    
-    }    
-     printf("%d",deque[r]);    
+    }     
 }    
 
 // delete_front() function deletes the element from the front    
-void delete_front()    
+void delete_front()  //#3  
 {    
-    if((f==-1) && (r==-1))    
-    {    
-        printf("Deque is empty");    
-    }    
-    else if(f==r)    
-    {    
+    if((f==-1) && (r==-1))  
+        printf("Deque is empty");  
+    else if(f==r){    
         printf("\nThe deleted element is %d", deque[f]);    
         f=-1;    
-        r=-1;    
-            
+        r=-1;         
     }    
-    //  else if(f==(size-1))    
-    //  {    //circular queue
-    //      printf("\nThe deleted element is %d", deque[f]);    
-    //      f=0;    
-    //  }    
-     else    
-     {    
+     else{    
           printf("\nThe deleted element is %d", deque[f]);    
           f=f+1;    
      }    
 }    
     
 // delete_rear() function deletes the element from the rear    
-void delete_rear()    
+void delete_rear()  //#4  
 {    
-    if((f==-1) && (r==-1))    
-    {    
-        printf("Deque is empty");    
-    }    
-    else if(f==r)    
-    {    
+    if((f==-1) && (r==-1))
+        printf("Deque is empty");     
+    else if(f==r){    
         printf("\nThe deleted element is %d", deque[r]);    
         f=-1;    
         r=-1;    
-            
-    }    
-    //  else if(r==0)    
-    // {    //circular list
-        //  printf("\nThe deleted element is %d", deque[r]);    
-        //  r=size-1;    
-    // }    
-     else    
-     {    
+    }
+     else{    
           printf("\nThe deleted element is %d", deque[r]);    
           r=r-1;    
      }    
 }    
-    
-int main()    
+
+// display function prints all the value of deque.    
+void display()  //#5  
 {    
-    insert_front(20);    
-    insert_front(10);    
-    insert_rear(30);    
-    insert_rear(50);    
-    insert_rear(80);    
-    display();  // Calling the display function to retrieve the values of deque    
+    int i=f;    
+    printf("\nElements in a deque are: ");
+    while(i!=r+1)
+    {    
+        printf("%d ",deque[i]);    
+        i=i+1;   
+    }      
+}    
+
+void main(){  
+    int ch=0,n;
+    printf("Enter no. of elements in Queue:");
+    scanf("%d",&n);
+    if(size<n)
+    printf("Plz I/P the elements less then %d",size);
+    else{
+    for(;;){//infinite loop
+    printf("\n f=%d r=%d\n",f,r);
+    printf("\n1 for insert front\n2 for insert rear\n3 for delete front\n4 for delete rear\n5 for Display\n6 for Existing\n");
+    scanf("%d",&ch);
+    switch(ch){
+    case 1:insert_front(n);break;
+    case 2:insert_rear(n);break; 
+    case 3:delete_front();break;
+    case 4:delete_rear();break;
+    case 5:display();break;
+    case 6:printf("Existing...");exit(0);
+    default:printf("Invalid input");
+    }//switch case
+    }//while
+    }//else
+}    //main
     
-    delete_front();    
-    delete_rear();    
-    display(); // calling display function to retrieve values after deletion   
-    return 0;    
-}     
+    // insert_front(20);    
+    // insert_front(10);    
+    // insert_rear(30);    
+    // insert_rear(50);    
+    // insert_rear(80);    
+    // display();  // Calling the display function to retrieve the values of deque    
+    
+    // delete_front();    
+    // delete_rear();    
+    // display(); // calling display function to retrieve values after deletion
