66940505d11100fbccf1d79813d1924231fe4dcc
Shikher-jain <shikherjain786@gmail.com>
2024-04-04 02:09:15 +0530

    python

--- .vscode/python/tempCodeRunnerFile.py
+++ .vscode/python/tempCodeRunnerFile.py
@@ -1,9 +1,7 @@
-import os
-import sys
-print(os.path,sys.executable)
-if(not os.path.exists("os MOdule")):
-    os.mkdir("os MOdule")
-
-for i in range(1,101):
-    os.mkdir(f"os MOdule/dayAS {i}")
-    # os.rename(f"os MOdule/day {i}",f"os MOdule/remane {i}")
+text="a c d e f g h i j k l m n o p q  s t u v w x y z"
+f=open("text.txt","w")
+f.write(text)
+f.close
+f=open("text.txt","r")
+print(f.read)
+f.close
